78357d940f262dbc1812b3bfdc89766df40aa3a5
David Poetzsch-Heffter <davidpoetzsch@web.de>
2015-12-25 19:23:22 +0100

    added check taint option

--- ASBDetection/asb_detection.cpp
+++ ASBDetection/asb_detection.cpp
@@ -13,6 +13,7 @@ namespace TaintAnalysis {
     struct ASBDetection : public ModulePass {
         static char ID;
         static cl::opt<bool> optionDumpTaint;
+        static cl::opt<bool> optionCheckTaint;
 
         ASBDetection() : ModulePass(ID) {}
 
@@ -29,15 +30,17 @@ namespace TaintAnalysis {
                 // dump the final taint results
                 DumpTaintVisitor dumper(10, vis);
                 dumper.visit(M);
+                // the module was not modified -> return false
                 return false;
             }
-            
+
             // the module was not modified -> return false
             return false;
         }
     };
 
     cl::opt<bool> ASBDetection::optionDumpTaint("asb_detection_dump_taint", cl::desc("Dump the statically analysed taint information"));
+    cl::opt<bool> ASBDetection::optionCheckTaint("asb_detection_check_taint", cl::desc("Check the static taint but do not instrument"));
     
     char ASBDetection::ID = 0;
     static RegisterPass<ASBDetection> X("asb_detection", "test function exist", false, false);
