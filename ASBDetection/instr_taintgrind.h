66b9195bdadbfd9ff0ee1a4c4349dfc252a3b769
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-01-10 14:18:16 +0100

    implemented moving of instructions

--- ASBDetection/instr_taintgrind.h
+++ ASBDetection/instr_taintgrind.h
@@ -1,6 +1,7 @@
 #ifndef TAINT_ANALYSIS_INSTR_TAINTGRIND_H
 #define TAINT_ANALYSIS_INSTR_TAINTGRIND_H
 
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstVisitor.h"
 
 using namespace llvm;
@@ -27,23 +28,33 @@ namespace TaintAnalysis {
             for (auto it = taintSources.begin(); it != taintSources.end(); ++it) {
                 // Ok, let's taint that value!
                 Instruction* taintSource = *it;
-                                
+
+                IRBuilder<> builder(getGlobalContext());
+                
                 // TODO 1. move all instructions after the source to a new BasicBlock
                 BasicBlock* startBlock = taintSource->getParent();
-                BasicBlock* endBlock = BasicBlock::Create(startBlock->getContext(), "instrEnd", startBlock->getParent());
+                BasicBlock* endBlock = BasicBlock::Create(getGlobalContext(), "instrEnd", startBlock->getParent());
                 bool move = false;
+                std::vector<Instruction*> toMove;
                 for (auto it2 = startBlock->begin(); it2 != startBlock->end(); ++it2) {
                     if (move) {
-//                        endBlock->getInstList().push_back(&(*it2));
+                        toMove.push_back(&(*it2));
                     } else {
                         move = (&(*it2) == taintSource);
                     }
                 }
 
-                endBlock->print(errs());
+                for (auto it3 = toMove.begin(); it3 != toMove.end(); ++it3) {
+                    (*it3)->removeFromParent();
+                    endBlock->getInstList().push_back(*it3);
+                }
+
+                endBlock->getParent()->print(errs());
                 errs() << "\n\n";
                                 
-                // 2. Store the value in a newly allocated memory cell
+                // TODO 2. Store the value in a newly allocated memory cell
+                builder.SetInsertPoint(endBlock);
+
                 Instruction* alloca = new AllocaInst(taintSource->getType(), "tmp", startBlock);
                 alloca->print(errs());
                 errs() << "\n";
@@ -54,6 +65,8 @@ namespace TaintAnalysis {
                 // TODO 3. insert the taintgrind instrumentation
 
                 // TODO 4. load the value from memory again and replace all usages of the taint source with the loaded value
+
+                break;
             }
             
             return !taintSources.empty();
