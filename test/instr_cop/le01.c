f805716a9d5b8bfc36f22e997a5cd380ad38173e
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-02-28 18:44:10 +0100

    added tests for the other comparison operators

--- test/instr_cop/le01.c
+++ test/instr_cop/le01.c
@@ -1,5 +1,5 @@
 int f(int a, int b) {
-    return a == b;
+    return a <= b;
 }
 
 int main() {
--- test/instr_cop/le04.c
+++ test/instr_cop/le04.c
@@ -1,9 +1,9 @@
 int f(int a, int b) {
-    return a == b;
+    return a <= b;
 }
 
 int main() {
     int a = 4;
     int b = 5;
-    return f(a, b) ? 42 : 23;
+    return f((int) &a, (int) &b) ? 42 : 23;
 }
