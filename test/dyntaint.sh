7c6977c50171b65af712cdc2bb59720bb63fbc38
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-06 17:45:49 +0100

    making naming of tmp files individual + auto cleanup by default

--- test/dyntaint.sh
+++ test/dyntaint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-DEST_LL="/tmp/dest.ll"
-DEST_EXE="/tmp/dest"
-DEST_TG="/tmp/dest.taintgrind.log"
-
 if [ -z "$1" ]; then
-    echo "Usage: dyntaint.sh <bitcode-src>"
+    echo "Usage: dyntaint.sh [args] <bitcode-src>"
+    echo "Args:"
+    echo "  -dt-noclean    Don't clean up afterwards leaving the tmp files in /tmp"
+    echo "  All other arguments are directly passed down to process-taintgrind-output"
     exit 1
 fi
+
+CLEANUP=1
 
 SRC="$1"
 ARGS=""
@@ -18,8 +19,34 @@ for ARG in "$@"; do
     SRC="$ARG"
 done
 
+PTO_ARGS=""
+
+for ARG in "$ARGS"; do
+    ARG_STRIPPED=`echo $ARG`
+    if [ "$ARG_STRIPPED" = "-dt-noclean" ]; then
+        CLEANUP=0
+    else
+        PTO_ARGS="$PTO_ARGS $ARG"
+    fi
+done
+
+SRC_BASE=`basename "$SRC" .ll`
+
+# generate random id
+#RID=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1`
+#BASE="$SRC_BASE_$RID"
+BASE="$SRC_BASE"
+
+DEST_LL="/tmp/$BASE.ll"
+DEST_EXE="/tmp/$BASE"
+DEST_TG="/tmp/$BASE.taintgrind.log"
+
 $(dirname $0)/asbdetect.sh -asb-log-level 0 -asb_detection_instr_only "$SRC" "$DEST_LL" && \
     clang -g -O0 -o "$DEST_EXE" "$DEST_LL" && \
     valgrind --tool=taintgrind --tainted-ins-only=yes "$DEST_EXE" 2> "$DEST_TG"
 
-exec $(dirname $0)/../process-taintgrind/process-taintgrind-output.rb $ARGS "$DEST_TG"
+$(dirname $0)/../process-taintgrind/process-taintgrind-output.rb $PTO_ARGS "$DEST_TG"
+
+if [ $CLEANUP = 1 ]; then
+    rm "$DEST_LL" "$DEST_EXE" "$DEST_TG"
+fi
