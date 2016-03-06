7ef0676c359e7ab4a121be922e07a5a2ce70ab8f
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-06 20:36:59 +0100

    added test folder for conditions

---
+++ test/instr_cond/tests.py
@@ -0,0 +1,25 @@
+"""
+tests.py for dynamic taint analysis tests
+"""
+
+# import the test infrastructure
+from infrastructure.tests import make_tests
+import os
+
+optionals = []
+
+def allTests():
+    """
+    This function returns a list of tests.
+    """
+    tests = make_tests("instr_cond", "dyntaint.sh", True, ["-no-color"])
+    
+    for test in tests:
+        # mark optionals
+        if test.getName() in optionals:
+            test.opt()
+
+        test.timeoutFactor = 3
+            
+    return tests
+
