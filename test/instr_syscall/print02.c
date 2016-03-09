911c593b8fe74624ba54ed41898ebc94ba4335f0
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-09 19:18:50 +0100

    basic syscall tests

---
+++ test/instr_syscall/print02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    printf("%d\n", (int) &i);
+    return 0;
+}
---
+++ test/instr_syscall/print03.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    printf("%d\n", i);
+    return 0;
+}
