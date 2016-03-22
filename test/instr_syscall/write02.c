9afa05fff271df8c00fb764da609659d5f0a2526
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-22 21:37:55 +0100

    added more in-depth write syscall tests

--- test/instr_syscall/write02.c
+++ test/instr_syscall/write02.c
@@ -1,11 +1,13 @@
-#include <string.h>
 #include <unistd.h>
 #include  <fcntl.h>
 
 int main() {
-    int i = 42;
+    int i = 0;
+    int* ips[2];
+    ips[0] = 0;
+    ips[1] = &i;
 
-    ssize_t nwritten = write(STDOUT_FILENO, &i, sizeof(i));
+    ssize_t nwritten = write(STDOUT_FILENO, &ips, sizeof(ips));
 
     return 0;
 }
--- test/instr_syscall/writev02.c
+++ test/instr_syscall/writev02.c
@@ -3,16 +3,24 @@
 #include <sys/uio.h>
 
 int main() {
-    char *str0 = "hello ";
-    char *str1 = "world\n";
+    int j = 0;
+    
+    int i[2];
+    i[0] = 42;
+    i[1] = 43;
+
+    int* ips[2];
+    ips[0] = 0;
+    ips[1] = &j;
+
     struct iovec iov[2];
     ssize_t nwritten;
 
-    iov[0].iov_base = str0;
-    iov[0].iov_len = strlen(str0);
-    iov[1].iov_base = str1;
-    iov[1].iov_len = strlen(str1);
-
+    iov[0].iov_base = &i;
+    iov[0].iov_len = sizeof(i);
+    iov[1].iov_base = &ips;
+    iov[1].iov_len = sizeof(ips);
+    
     nwritten = writev(STDOUT_FILENO, iov, 2);
 
     return 0;
