2e16e6733f026cd78933de044b1b75e77b0528a8
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-26 22:31:22 +0100

    implemented link flag for c++ linking

--- link.sh
+++ link.sh
@@ -2,9 +2,15 @@
 
 WRAPPERS_DIR=$(dirname "$0")/wrappers
 
+CMD="clang"
+if [ "$1" = "clang++" ];then
+    shift
+    CMD="clang++"
+fi
+
 if [ -z "$1" ]; then
     echo "Usage: link.sh [link-flags] <obj-files>*"
     exit 1
 fi
 
-exec clang -g -O0 -Wl,-wrap,malloc,-wrap,realloc,-wrap,calloc,-wrap,write $@ "$WRAPPERS_DIR"/libc_wrapper.o
+exec $CMD -g -O0 -Wl,-wrap,malloc,-wrap,realloc,-wrap,calloc,-wrap,write $@ "$WRAPPERS_DIR"/libc_wrapper.o
