2b46b42debefdcda1763e1afe46056964efa403a
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-03-28 23:14:51 +0200

    more work on get_traces

--- tgproc/src/graph/mod.rs
+++ tgproc/src/graph/mod.rs
@@ -2,6 +2,8 @@ mod tgnode;
 
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::iter::Iterator;
+use std::cmp::Ordering;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
@@ -13,7 +15,7 @@ use self::tgnode::TgNode;
 use self::tgnode::TgNodeMap;
 use super::cli::Options;
 
-const PRINT_TRACE_VERBOSITY: u8 = 20;
+const PRINT_DETECTION_VERBOSITY: u8 = 20;
 
 pub struct Graph<'a> {
     sinks : Vec<Rc<TgNode>>,
@@ -77,29 +79,68 @@ impl<'a> Graph<'a> {
         Ok(graph)
     }
 
-    pub fn get_traces(&self, sink: Rc<TgNode>) {
+    pub fn get_traces(&self, sink: &Rc<TgNode>) {
+        let print_detection = self.options.verbosity >= PRINT_DETECTION_VERBOSITY;
+        
         // we can't use recursion here because the graph can be VERY huge and the
         // stack depth is just not enough for that
-        //let mut sources = vec![];
+        let mut sources = vec![];
 
         // all nodes that still need to be processed
-        let mut queue = VecDeque::new();
+        let mut queue: VecDeque<&Rc<TgNode>> = VecDeque::new();
         queue.push_back(sink);
 
         // a map from detected nodes to their successors
-        //let mut detected = HashMap::new();
-        //detected.insert(sink, None);
+        let mut detected : HashMap<&Rc<TgNode>, Option<&Rc<TgNode>>> = HashMap::new();
+        detected.insert(sink, None);
 
-        while ! queue.is_empty() {
-            if self.options.verbosity >= PRINT_TRACE_VERBOSITY {
-                print!("{:>30}  ",
-                       format!("{:?}", queue.iter().map(|n| n.idx+1).collect::<Vec<usize>>()))
+        while let Some(op) = queue.pop_front() {
+            if print_detection {
+                print!("{:>30}  detecting {:2$}",
+                       format!("{:?}", queue.iter().map(|n| n.idx+1).collect::<Vec<usize>>()),
+                       op.idx+1,
+                       self.idxwidth);
+                match *detected.get(op).unwrap() {
+                    Some(ref successor) => print!(" from {:1$}  --  ", successor.idx+1, self.idxwidth),
+                    None => print!("{:1$}  --  ", " ", self.idxwidth + 6)
+                }
             }
 
-            let op = queue.pop_front();
+            if op.is_source() {
+                if print_detection { print!("found source") }
+                sources.push(op);
+            } else {
+                let all_preds : Vec<&Rc<TgNode>> = if op.is_sink() {
+                    op.sink_reasons.iter().collect()
+                } else {
+                    op.preds.iter().filter_map(|pred| pred.as_ref()).collect()
+                };
 
-            if self.options.verbosity >= PRINT_TRACE_VERBOSITY {
-                // TODO
+                // get all preds that are Some, non-green and haven't been detected yet
+                let (mut preds, skipped) : (Vec<&Rc<TgNode>>, Vec<&Rc<TgNode>>) = all_preds.iter()
+                    .partition(|&p| (! p.is_green()) && (! detected.contains_key(p)));
+
+                // put the red ones first
+                preds.sort_by(|a, b| {
+                    match (a.is_red(), b.is_red()) {
+                        (true, true) => Ordering::Equal,
+                        (true, false) => Ordering::Less,
+                        (false, true) => Ordering::Greater,
+                        (false, false) => Ordering::Equal
+                    }
+                });
+                
+                if print_detection {
+                    println!("adding preds {:?} (skipping {:?})",
+                             preds.iter().map(|p| p.idx + 1).collect::<Vec<usize>>(),
+                             skipped.iter().map(|p| p.idx + 1).collect::<Vec<usize>>());
+                }
+
+                for pred in preds.iter() {
+                    let x : &Rc<TgNode> = pred;
+                    queue.push_back(pred);
+                    detected.insert(pred, Some(op)); // link from where we found this one
+                }
             }
         }
     }
